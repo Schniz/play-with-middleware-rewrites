72ec62897a6c95529c70c1d02d794c37c7e550a7
Gal Schlezinger <gal@spitfire.co.il>
2022-01-18 16:51:07 +0200

    redirect

--- pages/api/_middleware.js
+++ pages/api/_middleware.js
@@ -1,39 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
-let counter = 0;
-const uid = crypto.randomUUID();
 const ALLOWED = ["allowed"];
 
 /**
  * @param {NextRequest} request
  */
 export default function middleware({ nextUrl }) {
-  if (nextUrl.pathname.endsWith("/uid")) {
-    return new Response(
-      JSON.stringify({
-        uid,
-        counter: counter++,
-      })
-    );
-  }
-  if (nextUrl.searchParams.has("clear")) {
-    for (const key of nextUrl.searchParams.keys()) {
-      if (!ALLOWED.includes(key)) {
-        nextUrl.searchParams.set(key, "");
-      }
-    }
-  } else if (nextUrl.searchParams.has("clear2")) {
-    return NextResponse.rewrite("/api/hello?");
-  } else {
-    for (const key of nextUrl.searchParams.keys()) {
-      if (!ALLOWED.includes(key)) {
-        nextUrl.searchParams.delete(key);
-      }
+  let changed = false;
+  for (const key of nextUrl.searchParams.keys()) {
+    if (!ALLOWED.includes(key)) {
+      nextUrl.searchParams.delete(key);
+      changed = true;
     }
   }
 
-  nextUrl.searchParams.set("appended", "true");
-  nextUrl.searchParams.set("_counter", String(++counter));
+  if (changed) {
+    return NextResponse.redirect(nextUrl);
+  }
 
   return NextResponse.rewrite(nextUrl);
 }
