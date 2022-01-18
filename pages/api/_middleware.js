4af11bcf465d19fa93a702bd62afee1a0ca5ad01
Gal Schlezinger <gal@spitfire.co.il>
2022-01-18 13:34:11 +0200

    deploy

--- pages/api/_middleware.js
+++ pages/api/_middleware.js
@@ -1,12 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
 let counter = 0;
+const uid = crypto.randomUUID();
 const ALLOWED = ["allowed"];
 
 /**
  * @param {NextRequest} request
  */
 export default function middleware({ nextUrl }) {
+  if (nextUrl.pathname.endsWith("/uid")) {
+    return new Response(
+      JSON.stringify({
+        uid,
+        counter: counter++,
+      })
+    );
+  }
   if (nextUrl.searchParams.has("clear")) {
     for (const key of nextUrl.searchParams.keys()) {
       if (!ALLOWED.includes(key)) {
