215e80634ac272fde98997b2bc2576df34fad58d
Gal Schlezinger <gal@spitfire.co.il>
2022-01-18 11:21:30 +0200

    test

--- pages/api/_middleware.js
+++ pages/api/_middleware.js
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
+let counter = 0;
 const ALLOWED = ["allowed"];
 
 /**
  * @param {NextRequest} request
  */
 export default function middleware({ nextUrl }) {
+  nextUrl.searchParams.set("_counter", ++counter);
   if (nextUrl.searchParams.has("clear")) {
     for (const key of nextUrl.searchParams.keys()) {
       if (!ALLOWED.includes(key)) {
