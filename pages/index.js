a2e193257dec4dbc0464707cdbd07b3f725ed185
Gal Schlezinger <gal@spitfire.co.il>
2022-01-20 13:35:22 +0200

    some test

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+export default function Home({ query }) {
+  return <pre>{JSON.stringify(query, null, 2)}</pre>;
+}
+
+/** @type {import('next').GetServerSideProps} */
+export const getServerSideProps = (context) => {
+  if (context.query.clear) {
+    console.log("OKEY");
+    context.res.writeHead(302, "Found", {
+      Location: "/?cleared=true",
+    });
+    return new Promise(() => {});
+  }
+
+  return {
+    props: {
+      query: context.query,
+    },
+  };
+};
